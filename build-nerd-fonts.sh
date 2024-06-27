1f9db73adefcc625fbe9b2f3fcc783fa28e220c3
Jero Ahumada <jero@iwell.info>
2024-06-26 18:35:40 -0700

    Updated to support alacritty

--- build-nerd-fonts.sh
+++ build-nerd-fonts.sh
@@ -2,14 +2,21 @@
 
 name="$([ "$#" -ge 1 ] && echo "$1" || echo "Ierovka")"
 dist_dir="dist/$name/TTF"
-rm -rf "$dist_dir"/*
 
-npm run build -- ttf::"$name"
+# rm -rf "${dist_dir:?}"/*
+# npm run build -- ttf::"$name"
 
 echo "Patching Nerd Fonts:"
 for f in "$dist_dir"/"$name"*ttf; do
-	if [[ "$f" == *"NerdFont"* ]]; then
-		continue
-	fi
-	fontforge -script font-patcher/font-patcher "$f" -o "$dist_dir"
+
+	[[ "$f" != *"NerdFont"* ]] && [ -f "$f" ] || continue
+
+	true && true || coninue
+
+	basename="$(basename "$f")"
+	new_name="$(echo "$basename" | sed 's/Ierovka-\(.*\).ttf/IerovkaNerdFont-\1/')"
+
+	echo "Patching nerd font '$f' to '$new_name'"
+
+	fontforge -script font-patcher/font-patcher "$f" -o "$dist_dir" --name "$new_name" --debug -c
 done
